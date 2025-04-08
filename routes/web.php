9a75dde02aa8ef22031096a9f329d1ffb6cde6a1
Arthur <arthur.santan4@hotmail.com>
2025-04-08 16:23:33 -0300

    005

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',function(){
     DB::connection()->getPdo();
+    echo 'Hmome';
 });
 
-Route::view('/teste', 'teste');
+Route::view('/teste', 'teste')->middleware('auth');
+
+Route::get('/login', function(){
+    echo 'formulario de login';
+})->name('login');
+
+Route::middleware('guest')->gourp(function(){
+    Route::get('/register', function(){
+        echo 'formulario de registro';
+    })->name('register');
+});
+
+Route::get('/register', function(){
+    echo 'formulario de registro';
+})->name('register');
